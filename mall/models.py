4198f3f295c333c9c6ec2fde59be8baf2711ec13
Soobin <soobin400@gmail.com>
2021-12-17 15:38:01 +0900

    Product 모델 만들기(수정)

--- mall/models.py
+++ mall/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    content = models.CharField(max_length=100, blank=True)
-    price = models.IntegerField()
-    material = models.CharField(max_length=100, blank=True)
-
-    image = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-    maker = models.ForeignKey(Maker, null=True, blank=True, on_delete=models.SET_NULL)
-
-
-    def __str__(self):
-        return f'[{self.pk}]{self.name} :: {self.category}'
-
-    def get_absolute_url(self):
-        return f'/mall/{self.pk}/'
-
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -50,3 +29,24 @@ class Maker(models.Model):
 
     def get_absolute_url(self):
         return f'/mall/maker/{self.pk}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.CharField(max_length=100, blank=True)
+    price = models.IntegerField()
+    material = models.CharField(max_length=100, blank=True)
+
+    image = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    maker = models.ForeignKey(Maker, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+    def __str__(self):
+        return f'[{self.pk}]{self.name} :: {self.category}'
+
+    def get_absolute_url(self):
+        return f'/mall/{self.pk}/'
+
+
